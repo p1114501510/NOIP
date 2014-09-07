0d3878f97f6e536311ba5f12a82df29a52d68e63
liuyun <liuyun@localhost.localdomain>
2014-09-07 17:22:10 +0800

    p2027.cpp

---
+++ p2027.cpp
@@ -0,0 +1,22 @@
+/*************************************************************************
+	> File Name: p2027.cpp
+	> Author: p1114501510
+	> Mail: p1114501510@163.com 
+	> Created Time: Sun 07 Sep 2014 05:16:50 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a,b,c;
+	cin>>a;
+	for(int i=0;i<a;i++){
+		cin>>b>>c;
+		if(b>=c)
+			cout<<"MMM BRAINS"<<endl;
+		else
+			cout<<"NO BRAINS"<<endl;
+	}
+	return 0;
+}
